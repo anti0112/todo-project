382e05cd2cd20fa47f0e509fec161dd8002df2c8
Danil Galkin <pixell_tm@bk.ru>
2022-10-11 21:21:04 +0300

    start server on VPS

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -2,6 +2,6 @@ from core.views import UserCreateView
 from django.urls import path
 
 urlpatterns = [
-    path('users/', UserCreateView.as_view(), name='register'),
+    path('sign-up/', UserCreateView.as_view(), name='register'),
     
 ]
