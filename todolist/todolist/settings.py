a72be4506e1340ec9d451d9006da4fe87eb8aadc
Danil Galkin <pixell_tm@bk.ru>
2022-10-26 15:18:51 +0300

    auth vk is working

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -51,10 +51,10 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "social_django.middleware.SocialAuthExceptionMiddleware",
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'todolist.urls'
