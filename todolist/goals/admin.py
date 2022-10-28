7ba2c21099fe20d8a11de4ffbc3a77af03edcb6b
Danil Galkin <pixell_tm@bk.ru>
2022-10-28 15:45:21 +0300

    CRUD GoalCategory

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from goals.models import GoalCategory
 
-# Register your models here.
+@admin.register(GoalCategory)
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created", "updated")
+    search_fields = ("title", "user")
+
--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from core.models import User
 from django.utils import timezone
 
+
 class DatesModelMixin(models.Model):
     class Meta:
         abstract = True
 
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+    created = models.DateTimeField(
+        verbose_name="Дата создания"
+    )
+    updated = models.DateTimeField(
+        verbose_name="Дата последнего обновления"
+    )
 
     def save(self, *args, **kwargs):
         if not self.id:  
@@ -22,24 +27,66 @@ class GoalCategory(DatesModelMixin):
         verbose_name_plural = 'Категории'
         
     title = models.CharField(
-        verbose_name = 'Название',
-        max_length = 255,
+        verbose_name='Название', max_length=255,
     )
     user = models.ForeignKey(
-        User, 
-        verbose_name = 'Автор',
-        on_delete = models.PROTECT
+        User, verbose_name='Автор', on_delete=models.PROTECT
     )
     is_deleted = models.BooleanField(
-        verbose_name = 'Удалена',
-        default = False, 
+        verbose_name='Удалена', default = False, 
     )
     
 class Goal(DatesModelMixin):
     class Meta:
         verbose_name = 'Цель'
         verbose_name_plural = 'Цели'
-        
     
+    class Status(models.IntegerChoices):
+        to_do = 1, 'К выполнению'
+        in_progress = 2, 'В процессе'
+        done = 3, 'Выполнено'
+        archived = 4, 'Архив'
+    
+    class Priority(models.IntegerChoices):
+        low = 1, 'Низкий'
+        medium = 2, 'Средний'
+        high = 3, 'Высокий'
+        critical = 4, 'Критический'
+        
+    category = models.ForeignKey(
+        GoalCategory, verbose_name='Категория', on_delete=models.PROTECT,
+    )
+    user = models.ForeignKey(
+        User, 
+        verbose_name='Пользователь',
+        related_name = 'goals',
+        on_delete=models.PROTECT,
+    )
+    title = models.CharField(
+        verbose_name='Заголовок', max_length=255,
+    )
+    description = models.CharField(
+        verbose_name='Описание', max_length=500,
+    )
+    due_date = models.DateField(
+        verbose_name='Дата выполнения', null=True, blank=True, default=None,
+    )
+    status = models.PositiveSmallIntegerField(
+        verbose_name = 'Статус', choices=Status.choices, default=Status.to_do,
+    )
+    priority = models.PositiveSmallIntegerField(
+        verbose_name='Приоритет', choices=Priority.choices, default=Priority.medium,    
+    )
+
+class Board(DatesModelMixin):
+    class Meta:
+        verbose_name = 'Доска'
+        verbose_name_plural = 'Доски'
+    
+    title = models.CharField(verbose_name='Название', max_length=255)
+    is_deleted = models.BooleanField(verbose_name='Удалена', default=False)
+    
+    def __str__(self):
+        return self.title
     
     
