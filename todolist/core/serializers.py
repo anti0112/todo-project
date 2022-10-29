dae3897e6b67ff5d335c74d007fe47805d6bb817
Danil Galkin <pixell_tm@bk.ru>
2022-10-29 17:59:28 +0300

    delete serializer user

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -85,15 +85,3 @@ class UpdatePasswordSerializer(serializers.ModelSerializer):
         instance.set_password(validated_data["new_password"])
         instance.save(update_fields=["password"])
         return instance
-    
-class UserCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-        
-    def create(self, validated_data):
-        user = User.objects.create(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-        
-        return user
