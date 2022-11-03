6b4f1d10cbf67196da2d241f9314030effa79d31
Danil Galkin <pixell_tm@bk.ru>
2022-11-03 12:28:46 +0300

    fix bug in serializer(validate_data not seen by the user)

--- todolist/goals/serializers/board.py
+++ todolist/goals/serializers/board.py
@@ -47,27 +47,42 @@ class BoardSerializer(serializers.ModelSerializer):
         read_only_fields = ("id", "created", "updated")
         fields = "__all__"
         
+class BoardSerializer(serializers.ModelSerializer):
+    participants = BoardParticipantSerializer(many=True)
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Board
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated")
+
     def update(self, instance, validated_data):
-        owner = validated_data.pop('user')
-        new_participant = validated_data.pop('participants')
-        new_id = {part['user'].id: part for part in new_participant.participants}
-        old_participants = instance.participants.exlude(user=owner)
+        owner = validated_data.pop("user")
         
         with transaction.atomic():
-            for old_participant in old_participants:
-                if old_participant.user_id not in new_id:
-                    old_participant.delete()
-                else:
-                    if old_participant.role != new_id[old_participant.user_id['role']]:
-                        old_participant.role = new_id[old_participant.user_id['role']]
-                        old_participant.save()
-                    new_id.pop(old_participants.user_id)
-            for new_part in new_id.values():
-                BoardParticipant.objects.create(
-                    board=instance, user=new_part['user'], role=new_part['role']
-                )
-            instance.title = validated_data['title']
+            if validated_data.get("participants"):
+                new_participants = validated_data.pop("participants")
+                new_by_id = {part["user"].id: part for part in new_participants}
+                old_participants = instance.participants.exclude(user=owner)
+
+                for old_participant in old_participants:
+                    if old_participant.user_id not in new_by_id:
+                        old_participant.delete()
+                    else:
+                        if old_participant.role != new_by_id[old_participant.user_id]["role"]:
+                            old_participant.role = new_by_id[old_participant.user_id]["role"]
+                            old_participant.save()
+                        new_by_id.pop(old_participant.user_id)
+
+                for new_part in new_by_id.values():
+                    BoardParticipant.objects.create(
+                        board=instance, user=new_part["user"], role=new_part["role"]
+                    )
+                    
+            if validated_data.get('title'):
+                instance.title = validated_data["title"]
             instance.save()
+
         return instance
 
                         
